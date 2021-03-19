e17a312a07ed1d91fbb815f1cdec2de558fc400e
MrPuppeteer <nuriksabagas@gmail.com>
2021-03-19 11:21:26 +0700

    add playRound

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,11 +3,23 @@
 function computerPlay() {
     const randomMove = Math.ceil(Math.random() * 3);
     if (randomMove === 1) {
-        return "Rock"
-    }
-    else if (randomMove === 2) {
-        return "Paper"
-    } else {return "Scissor"}
+        return "rock"
+    } else if (randomMove === 2) {
+        return "paper"
+    } else { return "scissor" }
 }
 
-console.log(computerPlay());
+// console.log(computerPlay());
+
+function playRound(plSelect, cpSelect) {
+    plSelect = plSelect.toLowerCase();
+    if (plSelect === "rock" && cpSelect === "rock" || plSelect === "paper" && cpSelect === "paper" || plSelect === "scissor" && cpSelect === "scissor") {
+        return "draw"
+    } else if (plSelect === "rock" && cpSelect === "scissor" || plSelect === "paper" && cpSelect === "rock" || plSelect === "scissor" && cpSelect === "paper") {
+        return "player win"
+    } else {return "computer win"}
+}
+
+const playerPlay = prompt();
+const game = playRound(playerPlay, computerPlay());
+console.log(game);
